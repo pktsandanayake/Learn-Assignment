3669b31fedcc5088a14cd1bd23ccd3528a42669a
pktsandanayake <pktsandanayake@outlook.com>
2025-03-27 14:06:20 +0530

    Added .env file

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,15 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./Route";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 const MOGO_URL =
   "mongodb+srv://sajani:Pktsanda730920@learn-assignment.ipd5a.mongodb.net/?appName=Learn-Assignment";
-
+console.log("URL", process.env.MOGO_URL);
 mongoose
-  .connect(MOGO_URL, { dbName: "Tasks" })
+  .connect(MOGO_URL, { dbName: process.env.dbName })
   .then(() => console.log("Database connected........"))
   .catch((error) => console.log(error));
 
@@ -20,6 +22,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.listen(4000, () =>
-  console.log("Server is running on http://localhost:4000")
+app.listen(process.env.PORT, () =>
+  console.log("Server is running on http://localhost:", process.env.PORT)
 );
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { getToDosByDate } from "../src/services/webApi";
+import axios from "axios";
 function App() {
-  return <div></div>;
+  const getToDosByDate = (Date: string) => {
+    console.log(Date);
+    const ApiBaseUrl = "http://localhost:4000";
+    axios
+      .get(ApiBaseUrl + "/todos/date/" + Date)
+      .then((data) => console.log("Data", data))
+      .catch((error) => console.log("error", error));
+  };
+  getToDosByDate("2025-03-24");
+  return (
+    <div>
+      <h1> testing....</h1>
+    </div>
+  );
 }
 
 export default App;
